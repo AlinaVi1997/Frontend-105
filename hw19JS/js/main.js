d56011cf1ea06ea75c2ff5b51d393eb7f06f1a0d
Alina <alinavyazovtseva@gmail.com>
2023-05-05 13:29:58 +0300

    hw19JS

---
+++ hw19JS/js/main.js
@@ -0,0 +1,17 @@
+
+// Comments
+/*Comments*/
+
+let name = prompt("Будь ласка введіть ваше імя");
+alert("Hello," + name);
+
+let answer = prompt('Enter your year of birth');
+const year = ('2023');
+let result = year - answer;
+alert(result.toFixed(0) + 'year'); 
+
+let side = prompt("Enter the lenght of the side of the square");
+let P = side * 4;
+alert("Perimeter of the square is  " + P);
+
+
